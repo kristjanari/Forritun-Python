657fb7c73859add5f158d237594e5824b19e9e08
Kristjan Ari Tomasson <kristjanarit@gmail.com>
2018-09-04 10:26:21 +0000

    Algorithm written up

---
+++ Tímaverkefni6/max_int.py
@@ -0,0 +1,6 @@
+#Búum til breytu max_int = 0
+#Búum til breytu n = 1, til ap komast inn í while lykkjuna
+#Biðjum notanda um að slá inn jákvæða heiltölu heiltölu
+#Ef hann slær inn jákvæða tölu athugum við hvort hún sé stærri en fyrri tölur, ef svo er þá verður hún max_int
+#ef hann slær inn neikvæða tölu hættum við og prentum max_int
+
