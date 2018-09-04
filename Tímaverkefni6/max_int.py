aedd98177b0180c23c6d8b7ae8c41ece06cfa487
Kristjan Ari Tomasson <kristjanarit@gmail.com>
2018-09-04 10:28:07 +0000

    Algoritm implemented

--- Tímaverkefni6/max_int.py
+++ Tímaverkefni6/max_int.py
@@ -4,3 +4,9 @@
 #Ef hann slær inn jákvæða tölu athugum við hvort hún sé stærri en fyrri tölur, ef svo er þá verður hún max_int
 #ef hann slær inn neikvæða tölu hættum við og prentum max_int
 
+max_int = 0
+n = 1
+while n > 0:
+    n = int(input("Input a number:"))
+    if n > max_int:
+        max_int = n
